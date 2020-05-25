e989fe6678ffafe063d30e4902a2b82f08a93c60
Dragon <1redyoung@gmail.com>
2020-05-25 16:55:59 -0400

    Add project files.

---
+++ HostOnlyCaptain/Patch.cs
@@ -0,0 +1,60 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace HostOnlyCaptain
+{
+    [HarmonyPatch(typeof(PLServer), "SetPlayerAsClassID")]
+    class Patch
+    {
+        static bool Prefix(ref int classID, ref int playerID)
+        {
+            if (classID == 0 && playerID != 0)
+            {
+                Debug.Log("[DragonWarning] Someone is trying to Become Captain without your permission. This could be okay.");
+                PLServer.Instance.AddNotification("[DragonWarning] Someone is trying to become captain without your permission This could be okay.", PLNetworkManager.Instance.LocalPlayerID, PLServer.Instance.GetEstimatedServerMs() + 6000, true);
+                return false;
+            }
+            if (classID != 0 && playerID == 0)
+            {
+                Debug.Log("[DragonWarning] Someone is trying to remove you from captain");
+                PLServer.Instance.AddNotification("[DragonWarning] Someone is trying to remove you from captain!", PLNetworkManager.Instance.LocalPlayerID, PLServer.Instance.GetEstimatedServerMs() + 6000, true);
+                return false;
+            }
+            if (classID < -1 || classID > 4)
+            {
+                Debug.Log("[DragonWarning] Someone is trying to set someone to a non-existant class. Class:" + classID);
+                PLServer.Instance.AddNotification("[DragonWarning] Someone is trying to someone to a non-existant class!", PLNetworkManager.Instance.LocalPlayerID, PLServer.Instance.GetEstimatedServerMs() + 6000, true);
+                return false;
+            }
+            return true;
+        }
+    }
+    [HarmonyPatch(typeof(PLServer), "RemoveBot")]
+    class RemoveBotPatch
+    {
+        static bool Prefix(ref int classID)
+        {
+            if (!PLServer.Instance.GetCachedFriendlyPlayerOfClass(classID).IsBot)
+            {
+                Debug.Log("[DragonWarning] Someone is trying to remove remove Player of class " + classID);
+                PLServer.Instance.AddNotification("[DragonWarning] Someone is trying to remove Player of class " + classID, PLNetworkManager.Instance.LocalPlayerID, PLServer.Instance.GetEstimatedServerMs() + 6000, true);
+                return false;
+            }
+            return true;
+        }
+    }
+    [HarmonyPatch(typeof(PLServer), "ServerRemoveCrewBotPlayer")]
+    class RemoveCrewBotPlayerPatch
+    {
+        static bool Prefix(ref int inPlayerID)
+        {
+            if (!PLServer.Instance.GetPlayerFromPlayerID(inPlayerID).IsBot)
+            {
+                Debug.Log("[DragonWarning] Someone is trying to delete PlayerID " + inPlayerID);
+                PLServer.Instance.AddNotification("[DragonWarning] Someone is trying to delete PlayerID " + inPlayerID, PLNetworkManager.Instance.LocalPlayerID, PLServer.Instance.GetEstimatedServerMs() + 6000, true);
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ HostOnlyCaptain/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using PulsarPluginLoader;
+
+namespace HostOnlyCaptain
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "1.2.1";
+
+        public override string Author => "Dragon";
+
+        public override string Name => "HostOnlyCaptain";
+
+        public override string HarmonyIdentifier()
+        {
+            return "Dragon.HostOnlyCaptain";
+        }
+    }
+}
